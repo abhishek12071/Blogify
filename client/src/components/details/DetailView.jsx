d4232928f1b4fd3926d888a0b114900ea4b9d212
abhishek <abhishek94601@gmail.com>
2023-07-06 20:57:23 +0530

    upadted detailview

--- client/src/components/details/DetailView.jsx
+++ client/src/components/details/DetailView.jsx
@@ -1,115 +1,132 @@
-import {useEffect, useState, useContext} from 'react';
-
-import {Box, Typography, styled} from '@mui/material';
-
-import {Edit, Delete} from '@mui/icons-material';
-
-import {useParams, Link, useNavigate} from 'react-router-dom';
-
-import {API} from '../../service/api';
-
-import {DataContext} from '../../context/DataProvider';
-
+import { useEffect, useState, useContext } from 'react';
+import { Box, Typography, styled } from '@mui/material';
+import { Edit, Delete } from '@mui/icons-material';
+import { useParams, Link, useNavigate } from 'react-router-dom';
+import { API } from '../../service/api';
+import { DataContext } from '../../context/DataProvider';
 import Comments from './comments/Comments';
 
-const Container=styled(Box)(({theme})=>({
-    margin:'50px 100px',
-    [theme.breakpoints.down('md')]:{
-        margin:0
-    }
+const Container = styled(Box)(({ theme }) => ({
+  margin: '50px 100px',
+  [theme.breakpoints.down('md')]: {
+    margin: 0,
+  },
 }));
 
 const Image = styled('img')({
-    widht:'100%',
-    height:'50vh',
-    objectFit:'cover'
-})
+  width: '100%',
+  height: '40vh',
+  objectFit: 'cover',
+  borderRadius: '10px',
+  boxShadow: '0 4px 10px rgba(0, 0, 0, 0.1)',
+});
 
 const Heading = styled(Typography)`
-font-size:38px;
-font-weight:600;
-text-align:center;
-margin: 50px 0 10px 0;
-word-break:break-word;
-`
-const EditIcon=styled(Edit)`
-margin: 5px;
-padding:5px;
-border:1px solid #878787;
-border-radius:10px;
-`
+  font-size: 38px;
+  font-weight: 600;
+  text-align: center;
+  margin: 50px 0 10px 0;
+  word-break: break-word;
+  color: #333;
+`;
 
-const DeleteIcon=styled(Delete)`
-margin: 5px;
-padding:5px;
-border:1px solid #878787;
-border-radius:10px;
-`
+const EditIcon = styled(Edit)`
+  margin: 5px;
+  padding: 5px;
+  border-radius: 50%;
+  color: #fff;
+  background-color: #ff4081;
+  transition: background-color 0.3s, transform 0.3s;
+  cursor: pointer;
 
-const Author=styled(Box)`
-color:#878787;
-margin:20px 0;
-display:flex;
-`
+  &:hover {
+    background-color: #f50057;
+    transform: scale(1.1);
+  }
+`;
 
-const Description=styled(Typography)`
+const DeleteIcon = styled(Delete)`
+  margin: 5px;
+  padding: 5px;
+  border-radius: 50%;
+  color: #fff;
+  background-color: #f44336;
+  transition: background-color 0.3s, transform 0.3s;
+  cursor: pointer;
 
-word-break:break-word`;
+  &:hover {
+    background-color: #d32f2f;
+    transform: scale(1.1);
+  }
+`;
 
-const DetailView=()=>{
-    const [post, setPost]=useState({});
-    const {id}=useParams();
-    const {account} = useContext(DataContext);
+const Author = styled(Box)`
+  color: #878787;
+  margin: 20px 0;
+  display: flex;
+  align-items: center;
+`;
 
-    const navigate = useNavigate();
-    const url= post.picture ? post.picture : 'https://images.unsplash.com/photo-1543128639-4cb7e6eeef1b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wJTIwc2V0dXB8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80';
+const Description = styled(Typography)`
+  word-break: break-word;
+  line-height: 1.5;
+  color: #555;
+`;
 
+const DetailView = () => {
+  const [post, setPost] = useState({});
+  const { id } = useParams();
+  const { account } = useContext(DataContext);
 
-    useEffect(()=>{
-        const fetchData=async()=>{
-            let response=await API.getPostById(id);
-            if(response.isSuccess){
-                setPost(response.data);
-            }
-        }
-        fetchData();
-    },[])
+  const navigate = useNavigate();
+  const url =
+    post.picture ||
+    'https://images.unsplash.com/photo-1543128639-4cb7e6eeef1b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wJTIwc2V0dXB8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80';
 
-    const deleteBlog = async()=>{
-        let response= await API.deletePost(post._id);
-        if(response.isSuccess){
-            navigate('/');
-        }
+  useEffect(() => {
+    const fetchData = async () => {
+      let response = await API.getPostById(id);
+      if (response.isSuccess) {
+        setPost(response.data);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const deleteBlog = async () => {
+    let response = await API.deletePost(post._id);
+    if (response.isSuccess) {
+      navigate('/');
     }
+  };
 
-    return (
-        <Container>
-            <Image src={url} alt='blog'/>
+  return (
+    <Container>
+      <Image src={url} alt="blog" />
 
-            <Box style={{float:'right'}}>
-                {
-                    account.username===post.username && 
-                    <>
-                        < Link to={`/update/${post._id}`}>
-                        <EditIcon color='primary'/>
-                        </Link>
-                        <DeleteIcon onClick={()=>deleteBlog()} color='error' />
-                    </>
-                }
-            </Box>
+      {account.username === post.username && (
+        <Box style={{ display: 'flex', justifyContent: 'flex-end' }}>
+          <Link to={`/update/${post._id}`}>
+            <EditIcon />
+          </Link>
+          <DeleteIcon onClick={deleteBlog} />
+        </Box>
+      )}
 
-            <Heading>{post.title}</Heading>
+      <Heading>{post.title}</Heading>
 
-            <Author>
-                <Typography>Author: <Box component='span' style={{fontWeight:600}}>{post.username}</Box></Typography>
-                <Typography style={{marginLeft: 'auto'}}>{new Date(post.createdDate).toDateString()}</Typography>
-            </Author>
+      <Author>
+        <Typography>
+          Author: <Box component="span" style={{ fontWeight: 600 }}>{post.username}</Box>
+        </Typography>
+        <Typography style={{ marginLeft: 'auto', color: '#888' }}>{new Date(post.createdDate).toDateString()}</Typography>
+      </Author>
 
-            <Description>{post.description}</Description>
+      <Description>{post.description}</Description>
 
-            <Comments post={post}/>
-        </Container>
-    )
-}
+      <Comments post={post} />
+    </Container>
+  );
+};
 
 export default DetailView;
